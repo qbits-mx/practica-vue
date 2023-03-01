c2fde91f2b35ffc97241c4a1e12ca927ce47fb4e
Gustavo Adolfo Arellano Sandoval <arellano.gustavo@gmail.com>
2023-03-01 14:56:40 -0600

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,42 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 import store from '@/store'
 import axios from 'axios';
 
-
-import Home from '@/views/HomeView'
-import UsaModal from '@/views/UsaModal'
-import Prueba from '@/views/PruebaView'
-import ProfileView from '@/views/ProfileView.vue'
-
-import Login from '@/views/LoginView'
 import Forbidden from '@/views/ForbiddenView'
-
-
-import Example1 from '@/views/Example1View'
-import Example2 from '@/views/Example2View'
-import Example3 from '@/views/Example3View'
-import Example4 from '@/views/Example4View'
-import ForgotPassView from '@/views/ForgotPassView'
-import RegisterView from '@/views/RegisterView'
-import RegisterConfirmView from '@/views/RegisterConfirmView'
-import UpdatePassView from '@/views/UpdatePassView'
-import TerminosCondicionesView from '@/views/TerminosCondicionesView'
-import RegisterSuccess from '@/views/RegisterSuccess'
-import ImageCropperView from '@/views/ImageCropperView'
-import NavigationDrawer from '@/components/NavigationDrawer'
-import NuevaImagen from '@/views/NuevaImagen'
-import UserImages from '@/views/UserImages'
-import NewImageProfile from '@/views/NewImageProfile'
-
+import Forbidden from '@/views/ForgotPassView'
+import Forbidden from '@/views/HomeView'
 import LoginPage from '@/views/LoginPage'
+import PageNotFound from '@/views/PageNotFound'
 import RegeneraClave from '@/views/RegeneraClave'
 import RegeneraClaveConfirma from '@/views/RegeneraClaveConfirma'
+import RegisterPage from '@/views/RegisterConfirmView'
 import RegisterPage from '@/views/RegisterPage'
-import VideoView from '@/views/VideoView'
-import InfoView from '@/views/InfoView'
-import PageNotFound from '@/views/PageNotFound'
-
-import TaskCard from '@/views/TaskViewNice'
-import PersonalData from '@/views/PersonalData'
+import RegisterPage from '@/views/RegisterSuccess'
+import RegisterPage from '@/views/UpdatePassView'
 
 const routes = [
   {
@@ -47,31 +22,9 @@ const routes = [
     meta: { allowedRoles: ['admin','regular'] }  
   },
   {
-    path: '/ui/personal-data',
-    name: 'personal-data',
-    component: PersonalData
-  }, 
-  {
-    path: '/ui/task',
-    name: 'task-card',
-    component: TaskCard
-  },  
-  {
     path: '/ui/regenera-clave',
     name: 'regenera-clave',
     component: RegeneraClave
-  },
-  {
-    path: '/ui/video-view',
-    name: 'video-view',
-    component: VideoView,
-    meta: { allowedRoles: ['admin','regular'] }
-  },
-  {
-    path: '/ui/info-view',
-    name: 'info-view',
-    component: InfoView,
-    meta: { allowedRoles: ['admin','regular'] }
   },
   {
     path: '/ui/regenera-clave-confirma',
@@ -84,61 +37,9 @@ const routes = [
     component: LoginPage
   },
   {
-    path: '/ui/login-page',
-    name: 'loginPage',
-    component: Login
-  },
-  {
     path: '/ui/forbidden',
     name: 'forbidden',
     component: Forbidden
-  },
-  {
-    path: '/ui/prueba',
-    name: 'prueba',
-    component: Prueba,
-    meta: { allowedRoles: ['admin','regular'] }
-  },
-  {
-    path: '/ui/usa-modal',
-    name: 'usa-modal',
-    component: UsaModal
-  },
-  {
-    path: '/profile',
-    name: 'profile',
-    component: ProfileView,
-    meta: { allowedRoles: ['regular'] }
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-    meta: { allowedRoles: ['admin','regular'] }
-  },
-  {
-    path: '/ui/example1',
-    name: 'example1',
-    component:Example1
-  },
-  {
-    path: '/ui/example2',
-    name: 'example2',
-    component:Example2
-  },
-  {
-    path: '/ui/example3',
-    name: 'example3',
-    component: Example3,
-    meta: { allowedRoles: ['admin','regular'] }
-  },
-  {
-    path: '/ui/example4',
-    name: 'example4',
-    component: Example4
   },
   {
     path: '/ui/forgotpass',
@@ -166,43 +67,9 @@ const routes = [
     component: UpdatePassView
   },
   {
-    path: '/ui/terminoscondiciones',
-    name: 'terminoscondiciones',
-    component: TerminosCondicionesView
-  },
-  {
     path: '/ui/registersuccess',
     name: 'registersuccess',
     component: RegisterSuccess
-  },
-  {
-    path: '/ui/imagecropper',
-    name: 'imagecropper',
-    component: ImageCropperView,
-    meta: { allowedRoles: ['admin','regular'] }
-  },
-  {
-    path: '/ui/menu',
-    name: 'menu',
-    component: NavigationDrawer,   
-  },
-  {
-    path: '/ui/nuevaimagen',
-    name: 'nuevaimagen',
-    component: NuevaImagen,
-    meta: { allowedRoles: ['admin','regular'] }
-  },
-  {
-    path: '/ui/userimages',
-    name: 'userimages',
-    component: UserImages,
-    meta: { allowedRoles: ['admin','regular'] }  
-  },
-  {
-    path: '/ui/imageprofile',
-    name: 'imageprofile',
-    component: NewImageProfile,
-    meta: { allowedRoles: ['admin','regular'] }  
   },
   {
     path: '/ui/reg',
@@ -211,7 +78,8 @@ const routes = [
   },
   { path: '/:pathMatch(.*)*', 
     name: 'NotFound', 
-    component: PageNotFound },
+    component: PageNotFound 
+  },
 ]
 
 
