e48a5c2fd89c4ecf8f436eef235471a9720e7646
Kixzeit <josh.jgp99@gmail.com>
2023-03-08 11:39:20 -0600

    hola

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,52 +1,82 @@
 <template>
     <div>
-        <h1>Login View XYZ</h1>
-        <h2 @mouseover="cambia">Gustavo</h2>
-        <button class="btn btn-danger" @click="carga" >Hola</button>
-        
-        <h2>{{ mensaje3 }}</h2>
+        <div class="muebles">
+    <h1 style="color: #f9ca24">Login View</h1>
 
-        <table class="table">
-            <thead>
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-                <th scope="col">Otra</th>
-                </tr>
-            </thead>
-            <tbody>
-            <tr v-for="experimento in mensaje4" v-bind:key="experimento.id">
-                <td>{{ experimento.id }}</td>
-                <td>{{ experimento.nombre }}</td>
-                <td>{{ experimento.color }}</td>
-                <td>{{ experimento.precio }}</td>
-                <td>{{ experimento.agotado }}</td>
-            </tr>
-            </tbody>
-        </table>
+    <div class="glitch-wrapper">
+      <div
+        @click="carga"
+        data-glitch="Kixzeit"
+        class="glitch"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#collapseWidthExample"
+        aria-expanded="false"
+        aria-controls="collapseWidthExample"
+      >
+        Kixzeit
+      </div>
+    </div>
 
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
+    <!--<table border="2" style="width: 70%">
+      <tr
+        v-for="parte in mensaje4"
+        v-bind:key="parte.id"
+        style="background-color: crimson"
+        class="collapse collapse-horizontal"
+        id="collapseWidthExample"
+      >
+        <td>{{ parte.id }}</td>
+        <td>{{ parte.nombre }}</td>
+        <td>{{ parte.color }}</td>
+        <td>{{ parte.precio }}</td>
+        <td>{{ parte.agotado }}</td>
+      </tr>
+    </table>-->
+
+    <table class="table table-dark table-striped" border="2" style="width: 70%">
+      <tr
+        v-for="parte in mensaje4"
+        v-bind:key="parte.id"
+        style="background-color: #f9ca24; color: black; font-weight: bold"
+        class="collapse collapse-horizontal"
+        id="collapseWidthExample"
+      >
+        <td>{{ parte.id }}</td>
+        <td>{{ parte.nombre }}</td>
+        <td>{{ parte.color }}</td>
+        <td>{{ parte.precio }}</td>
+        <td>{{ parte.agotado }}</td>
+      </tr>
+    </table>
+  </div>
+
+  <div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+
+  <div class="carousel-inner M-3">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="https://linuxhint.com/wp-content/uploads/2018/03/Skyline-1.jpg" alt="First slide">
+      <img src="https://linuxhint.com/wp-content/uploads/2018/03/Skyline-1.jpg" class="d-block w-50   m-auto m-2" alt="...">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://linuxhint.com/wp-content/uploads/2018/03/Earth-Horizon-1.jpg" alt="Second slide">
+      <img src="https://linuxhint.com/wp-content/uploads/2018/03/Earth-Horizon-1.jpg" class="d-block w-50 m-auto m-2" alt="...">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://linuxhint.com/wp-content/uploads/2018/03/roads-untraveled.jpg" alt="Third slide">
+      <img src="https://linuxhint.com/wp-content/uploads/2018/03/roads-untraveled.jpg" class="d-block w-50 m-auto m-2" alt="...">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
 
 
@@ -80,11 +110,88 @@ export default {
 }
 </script>
 
-<style scoped>
-h2 {
-    color:red;
-}
+<style>
 body {
-  background-color: yellow;
+  background-color: #4834d4;
+}
+
+td {
+  height: 20px;
+}
+
+.muebles {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.glitch-wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.glitch {
+  position: relative;
+  font-size: 80px;
+  font-weight: 700;
+  line-height: 1.2;
+  color: #eb4d4b;
+  letter-spacing: 5px;
+  z-index: 1;
+}
+
+.glitch:before,
+.glitch:after {
+  display: block;
+  content: attr(data-glitch);
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0.8;
+}
+
+.glitch:before {
+  animation: glitch-color 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) both
+    infinite;
+  color: rgb(250, 75, 6);
+  z-index: -1;
+}
+
+.glitch:after {
+  animation: glitch-color 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) reverse both
+    infinite;
+  color: #130f40;
+  z-index: -2;
+}
+
+@keyframes glitch-color {
+  0% {
+    transform: translate(0);
+  }
+
+  20% {
+    transform: translate(-3px, 3px);
+  }
+
+  40% {
+    transform: translate(-3px, -3px);
+  }
+
+  60% {
+    transform: translate(3px, 3px);
+  }
+
+  80% {
+    transform: translate(3px, -3px);
+  }
+
+  to {
+    transform: translate(0);
+  }
 }
 </style>
--- src/views/PageNotFoundView.vue
+++ src/views/PageNotFoundView.vue
@@ -1,38 +1,34 @@
 <template>
-    <div class="fondo">
-        <section class="error-container">
-        <span>4</span>
-        <span><span class="screen-reader-text">0</span></span>
-        <span>4</span>
-        </section>
-        <h1>404 Error Page</h1>
-        <p class="zoom-area">The page you are trying to see does not exist...</p>
-
-    </div>
+  <div class="fondo">
+    <section class="error-container">
+      <span>4</span>
+      <span><span class="screen-reader-text">0</span></span>
+      <span>4</span>
+    </section>
+    <h1>404 Error Page</h1>
+    <p class="zoom-area">The page you are trying to see does not exist...</p>
+  </div>
 </template>
 
-<style>
-body {
-    background-color:#2c3e50;
+<style scoped>
+.fondo{
+  color: #fff;
+  background-color: #a80c869f;
+  height: 100vh
 }
-.fondo {
-    color: #fff;
-    height: 100%;
-}
-@import url('https://fonts.googleapis.com/css?family=Montserrat:400,600,700');
-@import url('https://fonts.googleapis.com/css?family=Catamaran:400,800');
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,600,700");
+@import url("https://fonts.googleapis.com/css?family=Catamaran:400,800");
 .error-container {
   text-align: center;
   font-size: 180px;
-  font-family: 'Catamaran', sans-serif;
+  font-family: "Catamaran", sans-serif;
   font-weight: 800;
-  margin: 20px 15px;
 }
 .error-container > span {
   display: inline-block;
   line-height: 0.7;
   position: relative;
-  color: #FFB485;
+  color: #ffb485;
 }
 .error-container > span {
   display: inline-block;
@@ -40,11 +36,11 @@ body {
   vertical-align: middle;
 }
 .error-container > span:nth-of-type(1) {
-  color: #D1F2A5;
+  color: #d1f2a5;
   animation: colordancing 4s infinite;
 }
 .error-container > span:nth-of-type(3) {
-  color: #F56991;
+  color: #f56991;
   animation: colordancing2 4s infinite;
 }
 .error-container > span:nth-of-type(2) {
@@ -54,115 +50,121 @@ body {
 }
 .error-container > span:nth-of-type(2):before,
 .error-container > span:nth-of-type(2):after {
-	border-radius: 0%;
-	content:"";
-	position: absolute;
-	top: 0; left: 0;
-	width: inherit; height: inherit;
+  border-radius: 0%;
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: inherit;
+  height: inherit;
   border-radius: 999px;
-	box-shadow: inset 30px 0 0 rgba(209, 242, 165, 0.4),
-				inset 0 30px 0 rgba(239, 250, 180, 0.4),
-				inset -30px 0 0 rgba(255, 196, 140, 0.4),	
-				inset 0 -30px 0 rgba(245, 105, 145, 0.4);
+  box-shadow: inset 30px 0 0 rgba(209, 242, 165, 0.4),
+    inset 0 30px 0 rgba(239, 250, 180, 0.4),
+    inset -30px 0 0 rgba(255, 196, 140, 0.4),
+    inset 0 -30px 0 rgba(245, 105, 145, 0.4);
   animation: shadowsdancing 4s infinite;
 }
 .error-container > span:nth-of-type(2):before {
-	-webkit-transform: rotate(45deg);
-    -moz-transform: rotate(45deg);
-    transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
+  transform: rotate(45deg);
 }
 
 .screen-reader-text {
-    position: absolute;
-    top: -9999em;
-    left: -9999em;
+  position: absolute;
+  top: -9999em;
+  left: -9999em;
 }
 @keyframes shadowsdancing {
   0% {
     box-shadow: inset 30px 0 0 rgba(209, 242, 165, 0.4),
-				inset 0 30px 0 rgba(239, 250, 180, 0.4),
-				inset -30px 0 0 rgba(255, 196, 140, 0.4),	
-				inset 0 -30px 0 rgba(245, 105, 145, 0.4);
+      inset 0 30px 0 rgba(239, 250, 180, 0.4),
+      inset -30px 0 0 rgba(255, 196, 140, 0.4),
+      inset 0 -30px 0 rgba(245, 105, 145, 0.4);
   }
   25% {
     box-shadow: inset 30px 0 0 rgba(245, 105, 145, 0.4),
-				inset 0 30px 0 rgba(209, 242, 165, 0.4),
-				inset -30px 0 0 rgba(239, 250, 180, 0.4),	
-				inset 0 -30px 0 rgba(255, 196, 140, 0.4);
+      inset 0 30px 0 rgba(209, 242, 165, 0.4),
+      inset -30px 0 0 rgba(239, 250, 180, 0.4),
+      inset 0 -30px 0 rgba(255, 196, 140, 0.4);
   }
   50% {
-     box-shadow: inset 30px 0 0 rgba(255, 196, 140, 0.4),
-				inset 0 30px 0 rgba(245, 105, 145, 0.4),
-				inset -30px 0 0 rgba(209, 242, 165, 0.4),	
-				inset 0 -30px 0 rgba(239, 250, 180, 0.4);
+    box-shadow: inset 30px 0 0 rgba(255, 196, 140, 0.4),
+      inset 0 30px 0 rgba(245, 105, 145, 0.4),
+      inset -30px 0 0 rgba(209, 242, 165, 0.4),
+      inset 0 -30px 0 rgba(239, 250, 180, 0.4);
   }
   75% {
-   box-shadow: inset 30px 0 0 rgba(239, 250, 180, 0.4),
-				inset 0 30px 0 rgba(255, 196, 140, 0.4),
-				inset -30px 0 0 rgba(245, 105, 145, 0.4),	
-				inset 0 -30px 0 rgba(209, 242, 165, 0.4);
+    box-shadow: inset 30px 0 0 rgba(239, 250, 180, 0.4),
+      inset 0 30px 0 rgba(255, 196, 140, 0.4),
+      inset -30px 0 0 rgba(245, 105, 145, 0.4),
+      inset 0 -30px 0 rgba(209, 242, 165, 0.4);
   }
   100% {
     box-shadow: inset 30px 0 0 rgba(209, 242, 165, 0.4),
-				inset 0 30px 0 rgba(239, 250, 180, 0.4),
-				inset -30px 0 0 rgba(255, 196, 140, 0.4),	
-				inset 0 -30px 0 rgba(245, 105, 145, 0.4);
+      inset 0 30px 0 rgba(239, 250, 180, 0.4),
+      inset -30px 0 0 rgba(255, 196, 140, 0.4),
+      inset 0 -30px 0 rgba(245, 105, 145, 0.4);
   }
 }
 @keyframes colordancing {
   0% {
-    color: #D1F2A5;
+    color: #d1f2a5;
   }
   25% {
-    color: #F56991;
+    color: #f56991;
   }
   50% {
-    color: #FFC48C;
+    color: #ffc48c;
   }
   75% {
-    color: #EFFAB4;
+    color: #effab4;
   }
   100% {
-    color: #D1F2A5;
+    color: #d1f2a5;
   }
 }
 @keyframes colordancing2 {
   0% {
-    color: #FFC48C;
+    color: #ffc48c;
   }
   25% {
-    color: #EFFAB4;
+    color: #effab4;
   }
   50% {
-    color: #D1F2A5;
+    color: #d1f2a5;
   }
   75% {
-    color: #F56991;
+    color: #f56991;
   }
   100% {
-    color: #FFC48C;
+    color: #ffc48c;
   }
 }
 
 /* demo stuff */
 * {
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
 }
 body {
   background-color: #416475;
-  margin-bottom: 50px;
+  margin-bottom: 500px;
 }
-html, button, input, select, textarea {
-    font-family: 'Montserrat', Helvetica, sans-serif;
-    color: #92a4ad;
+html,
+button,
+input,
+select,
+textarea {
+  font-family: "Montserrat", Helvetica, sans-serif;
+  color: #92a4ad;
 }
 h1 {
   text-align: center;
   margin: 30px 15px;
 }
-.zoom-area { 
+.zoom-area {
   max-width: 490px;
   margin: 30px auto 30px;
   font-size: 19px;
@@ -174,15 +176,15 @@ h1 {
 a.more-link {
   text-transform: uppercase;
   font-size: 13px;
-    background-color: #92a4ad;
-    padding: 10px 15px;
-    border-radius: 0;
-    color: #416475;
-    display: inline-block;
-    margin-right: 5px;
-    margin-bottom: 5px;
-    line-height: 1.5;
-    text-decoration: none;
+  background-color: #92a4ad;
+  padding: 10px 15px;
+  border-radius: 0;
+  color: #416475;
+  display: inline-block;
+  margin-right: 5px;
+  margin-bottom: 5px;
+  line-height: 1.5;
+  text-decoration: none;
   margin-top: 50px;
   letter-spacing: 1px;
 }
